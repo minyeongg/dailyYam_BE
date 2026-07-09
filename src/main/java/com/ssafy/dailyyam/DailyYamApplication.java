03e275260dd2f77937a6b640bbd305ff5fa081a0
Minyeong Park <1030pmy@gmail.com>
2026-07-09 10:29:15 +0900

    Create DailyYamApplication.java

---
+++ src/main/java/com/ssafy/dailyyam/DailyYamApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.dailyyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyYamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyYamApplication.class, args);
+    }
+
+}
